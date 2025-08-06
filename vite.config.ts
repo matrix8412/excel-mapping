7f1c58c3c381c428b9a1ee8959f4c3d43d1432ec
matrix8412 <susek.martin@gmail.com>
2025-08-06 16:13:55 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ base: "/excel-mapping/" }) => {
     const env = loadEnv(mode, '.', '');
     return {
       define: {
